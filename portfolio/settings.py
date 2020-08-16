54f985811e79934cfa7b2435ee2bf76537f62ed3
Jiacheng Zhang <jiachengzhang1@email.arizona.edu>
2020-08-16 09:58:51 -0700

    handle static files

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -131,8 +131,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = ''
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = ( os.path.join('static'), )
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
