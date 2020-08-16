3100ef51e76f65a3e928b58a03baf49f3bb717dc
Jiacheng Zhang <jiachengzhang1@email.arizona.edu>
2020-08-16 14:36:42 -0700

    minor change

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config['DEBUG']
 
-ALLOWED_HOSTS = [config['HOST_IP']]
+ALLOWED_HOSTS = config['HOST_IP']
 
 # Application definition
 
