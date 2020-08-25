6e65b65ce36342f789a8410f7ccc0b2dde2f49bf
Jiacheng Zhang <jiachengzhang1@email.arizona.edu>
2020-08-25 16:58:22 -0700

    enhance styles and header

--- portfolio_web/static/js/sidebar.js
+++ portfolio_web/static/js/sidebar.js
@@ -1,13 +1,47 @@
-// handle navigation bar burger on click event
+// Common used DOM elements
+const dropdown = document.getElementById("sidebar-dropdown");
+const sidebarBars = document.getElementById("sidebar-bg");
+const sidebarBarsIcon = document.getElementById("sidebar-bg-icon");
+const header = document.getElementById("header");
+
+// Handle navigation bar burger on click event
 function handleOnClick() {
-    const dropdown = document.getElementById("sidebar-dropdown");
-    const sidebarBarsIcon = document.getElementById("sidebar-bg-icon");
     if (dropdown.classList.contains("show")) {
-        dropdown.classList.remove("show")
-        sidebarBarsIcon.className = "fas fa-bars"
+        dropdown.classList.remove("show");
+        sidebarBarsIcon.className = "fas fa-bars";
     } else {
         dropdown.classList.add("show");
-        sidebarBarsIcon.className = "fa-lg far fa-times"
+        sidebarBarsIcon.className = "fa-lg far fa-times";
     }
 }
 
+// Hide header when scroll down.
+// Show header when scroll up.
+let lastScrollTop = window.pageYOffset || document.documentElement.scrollTop;
+window.addEventListener("scroll", function(){
+    const st = window.pageYOffset || document.documentElement.scrollTop;
+    
+    if (st != lastScrollTop && dropdown.classList.contains("show")) {
+        dropdown.classList.remove("show");
+        sidebarBarsIcon.className = "fas fa-bars";
+    }
+    if (st > lastScrollTop + 50) {
+        header.classList.add("header-hidden");
+        lastScrollTop = st <= 0 ? 0 : st;
+    } 
+    else if (st < lastScrollTop - 10) {
+        header.classList.remove("header-hidden")
+        lastScrollTop = st <= 0 ? 0 : st;
+    }
+}, false);
+
+// Close the dropdown when click outside of it.
+document.addEventListener('click', function(event) {
+    const target = event.target;
+    const isClickInside = dropdown.contains(target);        
+
+    if (dropdown.classList.contains("show") && target != sidebarBarsIcon && !isClickInside) {
+        dropdown.classList.remove("show");
+        sidebarBarsIcon.className = "fas fa-bars";
+    }
+}, false);
